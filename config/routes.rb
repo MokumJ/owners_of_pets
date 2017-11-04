c1b330cbccb9a54648a60ca440300353d677d89c
dov11 <ovo.dan@gmail.com>
2017-11-04 14:06:56 +0100

    pets controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root "owner#index"
+get "pets" => "pets#pets"
+get "pets/show" => "pets#show", as: :pet
 end
