269c8062b04b3ae3fa4b5bf6e9b0f4cb5a29777f
MokumJ <jesse.vanderheijden@gmail.com>
2017-11-04 13:31:08 +0100

    update db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,13 @@ owner2 = Owner.create!(name: "Queen Elisabeth")
 owner3 = Owner.create!(name: "Ceasar Milan")
 
 Pet.create!([
-  {name: "Donadld Trump", age: rand(20), kind: "Dog", img_url: "https://hahahahaha." }
+  {name: "Donadld Trump", age: rand(20), kind: "Dog", img_url: "https://hahahahaha.", owner: owner1 },
+  {name: " Sint", age: rand(20), kind: "Dog", img_url: "https://hahahahaha.", owner: owner1  },
+  {name: "Donadld Trump", age: rand(20), kind: "Dog", img_url: "https://hahahahaha.", owner: owner1  },
+  {name: "Donadld Trump", age: rand(20), kind: "Dog", img_url: "https://hahahahaha.", owner: owner2 },
+  {name: "Donadld Trump", age: rand(20), kind: "Dog", img_url: "https://hahahahaha.", owner: owner2},
+  {name: "Donadld Trump", age: rand(20), kind: "Dog", img_url: "https://hahahahaha.", owner: owner3 },
+  {name: "Donadld Trump", age: rand(20), kind: "Dog", img_url: "https://hahahahaha.", owner: owner3 }
+
 
   ])
